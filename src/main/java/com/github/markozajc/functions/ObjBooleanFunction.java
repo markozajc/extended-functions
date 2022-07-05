eec1d1e0882625d39dbe9a1ed793f24d7ed18000
Marko Zajc <marko@zajc.eu.org>
2022-07-05 23:08:33 +0200

    Create {,E,AE}ObjBooleanFunction

---
+++ src/main/java/com/github/markozajc/functions/ObjBooleanFunction.java
@@ -0,0 +1,31 @@
+package com.github.markozajc.functions;
+
+import java.util.function.Function;
+
+/**
+ * A {@link Function} variant that takes a generic type and a primitive boolean, and
+ * returns a generic type.
+ *
+ * @author Marko Zajc
+ *
+ * @param <T>
+ *            the type of the first argument to the function
+ * @param <R>
+ *            the type of the result of the function
+ */
+@FunctionalInterface
+public interface ObjBooleanFunction<T, R> {
+
+	/**
+	 * Applies this function to the given arguments.
+	 *
+	 * @param t
+	 *            the first function argument
+	 * @param b
+	 *            the second ({@code boolean}) function argument
+	 *
+	 * @return the function result
+	 */
+	R apply(T t, boolean b);
+
+}

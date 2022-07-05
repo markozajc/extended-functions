9b2204f1dbbdab2792d08b91d84d18b7456bebf3
Marko Zajc <marko@zajc.eu.org>
2022-07-05 23:10:36 +0200

    Create ShortFunction

---
+++ src/main/java/com/github/markozajc/functions/ShortFunction.java
@@ -0,0 +1,27 @@
+package com.github.markozajc.functions;
+
+import java.util.function.Function;
+
+/**
+ * A {@link Function} variant that takes a primitive short, and returns a generic
+ * type.
+ *
+ * @author Marko Zajc
+ *
+ * @param <R>
+ *            the type of the result of the function
+ */
+@FunctionalInterface
+public interface ShortFunction<R> {
+
+	/**
+	 * Applies this function to the given arguments.
+	 *
+	 * @param value
+	 *            value the function argument
+	 *
+	 * @return the function result
+	 */
+	R apply(short value);
+
+}

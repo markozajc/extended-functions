0f9823d4776c5473a20388541d126dae615c3355
Marko Zajc <marko@zajc.eu.org>
2023-08-09 00:11:19 +0200

    Update the licensing information

--- src/main/java/org/eu/zajc/ef/Utilities.java
+++ src/main/java/org/eu/zajc/ef/Utilities.java
@@ -1,3 +1,19 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+/*
+ * extended-functions
+ * Copyright (C) 2021-2023 Marko Zajc
+ *
+ * This library is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free Software
+ * Foundation, version 3.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this
+ * program. If not, see <https://www.gnu.org/licenses/>.
+ */
 package org.eu.zajc.ef;
 
 import javax.annotation.Nonnull;
